7b4415e4a61641739dd3047c5e4ac6bb55f9c103
Alex Geis <siegxela@gmail.com>
2022-05-04 17:56:40 -0600

    half of gain env module

--- src/GainEnv.js
+++ src/GainEnv.js
@@ -1,43 +1,47 @@
 import React from "react";
 
 function GainEnv() {
-  var audioContext = new AudioContext();
-
   //FILTER MODULATION
   // each note has a highpass filter that sweeps from 10000 Hz to 500 Hz over the duration of the sound (linear).
   function handleButtonClick(e) {
     e.preventDefault();
-    var audioContext = new AudioContext();
+    var audioContext = new AudioContext()
 
-    play(0, 3, 0.5);
-    play(1, 10, 0.5);
-    play(2, 15, 0.5);
+play(0, 3, 0.5)
+play(1, 10, 0.5)
+play(2, 15, 0.5)
 
-    function play(delay, pitch, duration) {
-      var startTime = audioContext.currentTime + delay;
-      var endTime = startTime + duration;
+function play (delay, pitch, duration) {
+  var startTime = audioContext.currentTime + delay
+  var endTime = startTime + duration
 
-      var filter = audioContext.createBiquadFilter();
-      filter.connect(audioContext.destination);
-      filter.type = "highpass";
-      // preset value to avoid popping on start
-      filter.frequency.value = 10000;
-      // schedule the start time
-      // setValueAtTime(value, time)
-      filter.frequency.setValueAtTime(10000, startTime);
+  var oscillator = audioContext.createOscillator()
+  var amp = audioContext.createGain()
+amp.connect(audioContext.destination)
+oscillator.connect(amp)
 
-      // ramp the value, and set end time!
-      filter.frequency.linearRampToValueAtTime(500, endTime);
-      //   filter.frequency.exponentialRampToValueAtTime(500, endTime);
+//GAIN NODE - single autrribute, an Audioparam called gain
+//halve the volume
+//amp.gain.value = 0.5
 
-      var oscillator = audioContext.createOscillator();
-      oscillator.connect(filter);
+//SCHEDULING ATTACK AND RELEASE
+// audioParam.setTargetAtTime(targetValue, startTime, timeConstant)
+// attack ranges from 0 to 1
 
-      oscillator.type = "sawtooth";
-      oscillator.detune.value = pitch * 100;
+//"soft" attack - starts at 0
+amp.gain.value = 0
+//the target is 1, starts when the audio context starts, and decay rate is 0.1 seconds
+amp.gain.setTargetAtTime(1, audioContext.currentTime, 0.1)
+//Release - sweep value back to 0, starting at the endTime, with an exponential decay rate defined by timeConstant, or 0.2
+amp.gain.setTargetAtTime(0, endTime, 0.2)
 
-      oscillator.start(startTime);
-      oscillator.stop(endTime);
+  oscillator.type = 'sawtooth'
+  oscillator.detune.value = pitch * 100
+
+  oscillator.start(startTime)
+  oscillator.stop(endTime + 2)
+}
+
     }
   }
 
